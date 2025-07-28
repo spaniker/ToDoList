96bef1a2d97f292255a6f338acad03bff0a670b8
spaniker <84048769+spaniker@users.noreply.github.com>
2025-07-28 09:56:58 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Syona Paniker on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
